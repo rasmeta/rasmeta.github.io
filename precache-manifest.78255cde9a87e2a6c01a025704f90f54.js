03fa0cc8234c172e62425c800ccf2fb82f80f084
Piotr Król <plo@kmd.dk>
2019-07-12 16:36:13 +0200

    where am I 1.0

---
+++ precache-manifest.78255cde9a87e2a6c01a025704f90f54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea3c23cddff0467b87b720683af1f1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "664acf1d71483fb68788",
+    "url": "/static/css/main.74c3e7a6.chunk.css"
+  },
+  {
+    "revision": "80126425304fdab63720",
+    "url": "/static/js/2.547a994a.chunk.js"
+  },
+  {
+    "revision": "664acf1d71483fb68788",
+    "url": "/static/js/main.7b330378.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
